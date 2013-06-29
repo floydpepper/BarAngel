6d2245c9faf997028087959394a5834957405b0f
Michael Lynch <mick.lynch@gmail.com>
2013-06-29 17:24:38 -0400

    First commit

---
+++ BarAngel/src/main/java/com/example/barangel/BeerClass.java
@@ -0,0 +1,27 @@
+package com.example.barangel;
+
+/**
+ * Created by mlynch on 29/06/13.
+ */
+public class BeerClass {
+
+    float mPrice;
+    float mAlcohol;
+    int mCalories;
+
+    public int setPrice(float p) {
+        mPrice = p;
+        return 0;
+    }
+
+    public int setAlcohol(float alcohol) {
+        mAlcohol = alcohol;
+        return 0;
+    }
+
+    public int setCalories(int calories) {
+        mCalories = calories;
+        return 0;
+    }
+
+}
